7612aa07f2d62bd4fc99c9f8f822bc58f8458f55
kxxsu <m.c.fidan@live.de>
2020-12-21 21:12:18 +0100

    Command Factory in Progress - choosing list type complete

--- app/src/main/java/AlgoUeb2/commands/CommandFactory.java
+++ app/src/main/java/AlgoUeb2/commands/CommandFactory.java
@@ -9,38 +9,70 @@ public class CommandFactory {
 
     private static Listable<Student> list;
 
+    private static final String SINGLE_MENU = "Use a SinglyLinkedList.";
+    private static final String DOUBLE_MENU = "Use a DoublyLinkedList.";
+    private static final String CHOSEN_SINGLE = "You have chosen a SinglyLinkedList.";
+    private static final String CHOSEN_DOUBLE = "You have chosen a DoublyLinkedList.";
+    private static final String EXIT_MENU = "Exit";
+    private static final String EXIT = "See you another time!";
+
+
+
     public static DoublyLinkedList<ICommand> returnsCommandList() {
         DoublyLinkedList<ICommand> list = new DoublyLinkedList<>();
 
         return list;
     }
 
-    private static ICommand singlyListChosen() {
+    private static DoublyLinkedList<ICommand> chooseListType() {
+        DoublyLinkedList<ICommand> list = new DoublyLinkedList<>();
+        list.add(exit());
+        list.add(chooseSinglyList());
+        list.add(chooseDoublyList());
+        return list;
+    }
+
+    private static ICommand exit() {
         return new ICommand() {
             @Override
             public String execute() {
-                list = new SinglyLinkedList<>();
-                return null;
+                System.exit(0);
+                return EXIT;
             }
 
             @Override
             public String description() {
-                return null;
+                return EXIT_MENU;
             }
         };
     }
 
-    private static ICommand doublyListChosen() {
+    private static ICommand chooseSinglyList() {
         return new ICommand() {
             @Override
             public String execute() {
-                list = new DoublyLinkedList<>();
-                return null;
+                list = new SinglyLinkedList<>();
+                return CHOSEN_SINGLE;
             }
 
             @Override
             public String description() {
-                return null;
+                return SINGLE_MENU;
+            }
+        };
+    }
+
+    private static ICommand chooseDoublyList() {
+        return new ICommand() {
+            @Override
+            public String execute() {
+                list = new DoublyLinkedList<>();
+                return CHOSEN_DOUBLE;
+            }
+
+            @Override
+            public String description() {
+                return DOUBLE_MENU;
             }
         };
     }
